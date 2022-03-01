b1d6b86e2f7843cb64a9ff6272747272e31b0006
bisper <bisper@nate.com>
2022-03-01 11:44:40 +0900

    템플릿 문자열 사용하기.

---
+++ src/es6/02-2.js
@@ -0,0 +1,37 @@
+var string1 = "안녕하세요!";
+var string2 = "반갑습니다";
+var greeting = `${string1} ${string2}`;
+var product = { name: "도서", price: "4200원" };
+var message = `제품 ${product.name}의 가격은 ${product.price}입니다`;
+var multiLine = `문자열1
+문자열 2`;
+
+var value1 = 1;
+var value2 = 2;
+var boolValue = false;
+var operator1 = `곱셈값은 ${value1 * value2}입니다.`;
+var operator2 = `${boolValue ? "참" : "거짓"}입니다.`;
+
+console.log(string1);
+console.log(string2);
+console.log(greeting);
+console.log(product);
+console.log(message);
+console.log(multiLine);
+
+console.log(value1);
+console.log(value2);
+console.log(boolValue);
+console.log(operator1);
+console.log(operator2);
+
+var cart = { name: "도서", price: 1500 };
+var getTotal = function (cart) {
+  return `${cart.price} 원`;
+};
+
+var myCart = `장바구니에 ${cart.name} 가 있습니다. 총 금액은 ${getTotal(
+  cart
+)} 입니다.`;
+
+console.log(myCart);
