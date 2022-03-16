7e71b5e5a84144bb96ff1f93648286892e1ed06f
bisper1 <bisper@naver.com>
2022-03-16 19:04:07 +0900

    클래스형 컴포넌트에서 문자열이 아닌 데이터 하위 컴포넌트에 전송

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import React from "react";
 import TodaysPlan from "./03/TodaysPlan";
 // 프로퍼티 사용해 보기
 import PropsComponent from "./03/PropsComponent";
+import ChildComponent from "./03/ChildComponent";
 
 class App extends React.Component {
   render() {
@@ -23,8 +24,21 @@ class App extends React.Component {
         <img src="http://www.easyspub.co.kr/images/logo_footer.png" alt="" />
         <div>안녕하세요</div>
         <div className="body">
+          {/* 컴포넌트 사용하기 */}
           <TodaysPlan />
+          {/* 하위 컴포넌트에 프로퍼티로 문자열 데이터 전송 */}
           <PropsComponent name="두잇 리액트" />
+          {/* 하위 컴포넌트에 프로퍼티로 문자열이 아닌 데이터 전송 */}
+          <ChildComponent
+            boolValue={true}
+            numValue={1}
+            arrayValue={[1, 2, 3]}
+            objValue={{ name: "제목", age: 30 }}
+            nodeValue={<h1>노드</h1>}
+            funcValue={() => {
+              console.log("메시지");
+            }}
+          />
         </div>
       </div>
     );
